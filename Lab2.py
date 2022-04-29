9931304e29d21c0f3b60921664fd1fe2195dc3c2
{chonglin} <{tehchonglin@gmail.com}>
2022-04-29 16:08:33 +0800

    Version 1.011

--- Lab2.py
+++ Lab2.py
@@ -1,2 +1,34 @@
 def display_main_menu():
-    print("display_main_menu")
+    print("Enter some numbers seperated by commas")
+
+def get_user_input():
+    number = input()
+    number_list = number.split(",")
+    float_list = [float(a) for a in number_list]
+    print(float_list)
+    return float_list
+
+def calc_average_temperature(numbersss):
+    total = 0
+    for number in numbersss:
+        total = total + number
+    average = total/len(numbersss)
+    print("The average is ", average)
+
+def calc_min_max_temperature(numbersss):
+    maximum = max(numbersss)
+    minimum = min(numbersss)
+    print("The maximum is " ,maximum)
+    print("The minimum is ", minimum)
+
+
+
+def main():
+    display_main_menu()
+    numbersss = get_user_input()
+    calc_average_temperature(numbersss)
+    calc_min_max_temperature(numbersss)
+
+
+
+main()
